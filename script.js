7fd23682098b73608c7e961fa448cf8a68976c78
Andrew Furth <andrew.furth@gmail.com>
2016-09-13 15:09:43 -0700

    css fixed so 'current' column item is displayed, and other is hidden

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ $(function() {
       $startBtn = $("#start-btn");
 
   // Default: set the first number to 'current'
-  $(".column-box .col:first-child").addClass("current");
+  $(".column-box .col-item:last-child").addClass("current").css("top", 0);
+  $(".col-item").not(".current").css("top", 200)
 
   $startBtn.on("click", function() {
     console.log("CLICKED")
