8f1c0d6b377ad47e3458f27ee90134ff05716d7c
Andrew Furth <andrew.furth@gmail.com>
2016-09-19 15:25:05 -0700

    new spin function implemented; html and css updated

--- script.js
+++ script.js
@@ -1,101 +1,58 @@
-// 1. spinItem function (consider name)
-// 2. Set names of item divs in each row
-// 3. Abstract function for initiateSpin for each column
+$( document ).ready(function() {
+  // Clone the first element; append to end of list
+
+  // Reason? --> As the final item is exiting the viewport
+  // the next item (which is realy the first item), must
+  // 'appear' to be entering the frame.
+
+  // Under the hood, there is a jump between the last
+  // <li> entering the frame, and then the entire list
+  // resetting and the first <li> filling the frame.
+  // However since the first and last item are identical
+  // the 'jump' is imperceptible to the user
+
+  var itemList = $('#col-one>ul:first')
+  var firstItem = itemList.find('li:first')
+  firstItem.clone().appendTo(itemList)
 
 
+  // 1. Pass in a count for the number of spins
+  // 2. .stop() allows us to ensure that any effects
+      // effects of previous spin animation are killed
+  // 3. Animates the list sliding upwards, creating a 
+      // scroll effect
+  // 4. IF still iterations left in count...
+      // repeat spin animation (count decrements)
+    // ELSE if count is at 0 and this is final iteration
 
-// Function updates the class and animates change to
-// CSS position for items
-function spinItem(el, speed) {
-  if (el.hasClass("shown")) {
-  // If currently class is "shown", 
-    // 1. change class to "below" 
-    // 2. and animate top to 200px;
-    // (simulates sliding down out of frame)
-    el.removeClass("shown")
-    el.animate({
-      "top": 200
-    }, speed)
-    el.addClass("last")
-  } else if (el.hasClass("last")) {
-  // If currently class is "below", 
-    // 1. change class to "above" 
-    // 2. and change top to -200px;
-    el.removeClass("last").css("top", -200).addClass("next")
-  } else {
-  // If currently class is "above", 
-    // 1. change class to "show" 
-    // 2. and animate top to 0px;
-    // (simulates sliding down into the fram)
-    el.removeClass("next")
-    el.animate({
-      "top": 0
-    }, speed)
-    el.addClass("shown")
+  function spin(count) {
+    $('#col-one').stop().animate({
+        top: -600
+    }, 2000, 'linear', function () {
+        if (count == 0) {
+            var slot = Math.floor(Math.random() * 3),
+                top = -slot * 200,
+                time =  2000 * slot / 3;
+            console.log(count, slot, top, time)
+            $(this).css({
+                top: 0
+            }).animate({
+                top: top
+            }, time, 'linear')
+        } else {
+            $(this).css({
+                top: 0
+            })
+            spin(count - 1)
+        };
+    });
   }
-}
 
-// Initiates the spinning for each column with
-// a random number of spins
-function initiateSpin(item1, item2, item3) {
-  // Determine number of spins and set count
-  var spinTotal = Math.floor(Math.random() * 10);
-  var counter = 0;
-  
-  // Determine speed of spin
-  var speed = Math.floor(Math.random()*1000) + 500;
+  $('#start-btn').click(function () {
+      $('#col-one').css({
+          top: 0
+      }) 
+      spin(1)
+  });
 
-  // console.log("Number of spins is ", spinTotal)
-  var refreshId = setInterval(() => {
-    if (counter === spinTotal) {
-      clearInterval(refreshId)
-      // console.log("INTERVAL STOPPED")
-    }
-    spinItem(item1, speed);
-    spinItem(item2, speed);
-    spinItem(item3, speed);
-    counter++;
-    // console.log("counter is ", counter)
-
-  // At the moment the interval time needs to be larger 
-  // than the animation speed or the animation breaks
-  }, 0)
-}
-
-// This function will automate setting the class for each
-// column
-function setClass(itm1, itm2, itm3) {
-  itm1.addClass("shown").css("top", 0);
-  itm2.addClass("next").css("top", -200);
-  itm3.addClass("last").css("top", -200);  
-}
-
-$(function() {
-  var speedOne = 1000,
-      $startBtn = $("#start-btn");
-
-  // Column One
-  $oneOne = $("#col-one .item-one");
-  $oneTwo = $("#col-one .item-two");
-  $oneThree = $("#col-one .item-three");
-  // Column Two
-  $twoOne = $("#col-two .item-one");
-  $twoTwo = $("#col-two .item-two");
-  $twoThree = $("#col-two .item-three");
-  // Column Three
-  $threeOne = $("#col-three .item-one");
-  $threeTwo = $("#col-three .item-two");
-  $threeThree = $("#col-three .item-three");
-
-  // Set classes for each column
-  setClass($oneOne, $oneTwo, $oneThree);
-  setClass($twoOne, $twoTwo, $twoThree);
-  setClass($threeOne, $threeTwo, $threeThree);
-
-
-  $startBtn.on("click", function() {
-    initiateSpin($oneOne, $oneTwo, $oneThree);
-    initiateSpin($twoOne, $twoTwo, $twoThree);
-    initiateSpin($threeOne, $threeTwo, $threeThree);
-  })
 });
